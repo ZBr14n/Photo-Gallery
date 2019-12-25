393b7a8e6e5b52783b7c8367b588292996090f1c
ZBr14n <lamb2@wit.edu>
2019-12-24 23:33:29 -0500

    test new url background image

--- resources/views/photography/nyc.blade.php
+++ resources/views/photography/nyc.blade.php
@@ -10,8 +10,12 @@
         
     <div class="content-wrapper">
         <div class="nyc-title"><h1>New York City</h1></div>
-        <div class="photos-wrapper">            
-            <img src="/assets/images/photos/nyc/tower4.jpg" alt="Photo" />
+        <div class="photos-wrapper">      
+            <img src={{asset('storage/images/photos/nyc/tower4.jpg')}} alt="Photo" />
+            <img src={{asset('storage/images/photos/nyc/cube.jpg')}} alt="Photo" />
+            <img src={{asset('storage/images/photos/nyc/museum.jpg')}} alt="Photo" />
+
+            {{-- <img src="/assets/images/photos/nyc/tower4.jpg" alt="Photo" />
             <img src="/assets/images/photos/nyc/cube.jpg" alt="Photo" />
             <img src="/assets/images/photos/nyc/museum.jpg" alt="Photo" />        
             <img src="/assets/images/photos/nyc/tower.jpg" alt="Photo" />
@@ -20,7 +24,7 @@
             <img src="/assets/images/photos/nyc/soho.jpg" alt="Photo" />
             <img src="/assets/images/photos/nyc/shrine.jpg" alt="Photo" />
             <img src="/assets/images/photos/nyc/park.jpg" alt="Photo" />
-            <img src="/assets/images/photos/nyc/columbia_uni.jpg" alt="Photo" />
+            <img src="/assets/images/photos/nyc/columbia_uni.jpg" alt="Photo" /> --}}
             
         </div>
     </div>
