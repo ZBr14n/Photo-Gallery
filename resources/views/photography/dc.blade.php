537dd0f4f102e13f6d013415c595d5f5326c5b93
ZBr14n <lamb2@wit.edu>
2019-12-25 09:16:55 -0500

    test images uplaod

--- resources/views/photography/dc.blade.php
+++ resources/views/photography/dc.blade.php
@@ -16,7 +16,26 @@
         <br />
         <br />
         <div class="photos-wrapper">  
-            <img src={{asset('storage/images/photos/dc/cherry_blossom.jpg')}} alt="Photo" />
+            <img src="{{asset('images/cherry_blossom.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/monument.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/jefferson.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/lincoln.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/mlk.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/monument2.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/museum.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_5028.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4975.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4974.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4907.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4780.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4661.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4758.jpg')}}" alt="Photo" />
+            <img src="{{asset('images/IMG_4966.jpg')}}" alt="Photo" />
+
+
+
+
+            {{-- <img src={{asset('storage/images/photos/dc/cherry_blossom.jpg')}} alt="Photo" />
             <img src={{asset('storage/images/photos/dc/monument.jpg')}} alt="Photo" />
             <img src={{asset('storage/images/photos/dc/jefferson.jpg')}} alt="Photo" />
             <img src={{asset('storage/images/photos/dc/lincoln.jpg')}} alt="Photo" />
@@ -31,7 +50,7 @@
             <img src={{asset('storage/images/photos/dc/IMG_4780.jpg')}} alt="Photo" />            
             <img src={{asset('storage/images/photos/dc/IMG_4661.jpg')}} alt="Photo" />
             <img src={{asset('storage/images/photos/dc/IMG_4758.jpg')}} alt="Photo" />
-            <img src={{asset('storage/images/photos/dc/IMG_4966.jpg')}} alt="Photo" />
+            <img src={{asset('storage/images/photos/dc/IMG_4966.jpg')}} alt="Photo" /> --}}
 
 
 
